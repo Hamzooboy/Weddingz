03241e4cd481e406d9f68a70c463ae5ed0d4af7a
Hamzooboy <awanhamza102@gmail.com>
2022-06-01 17:23:19 +0500

    Checking

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -6,7 +6,7 @@ const bookingController = require('../controllers/bookingController')
 const router = express.Router();
 
 
-router.get('/', bookingController.createBookingCheckout, authController.protect, viewControllers.getOverview)
+router.get('/', bookingController.createBookingCheckout, viewControllers.getOverview)
 
 
 
