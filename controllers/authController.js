8ed4c7b041fda0cde0431e0130b1b22f0dca3ca4
Hamzooboy <awanhamza102@gmail.com>
2022-06-05 21:08:34 +0500

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -182,9 +182,10 @@
          //  console.log(req.headers.authorization.startsWith('Bearer'));
 
          token = req.headers.authorization.split(' ')[1];
-     } else if (req.cookies.jwt) {
-         token = req.cookies.jwt
      }
+     //  else if (req.cookies.jwt) {
+     //      token = req.cookies.jwt
+     //  }
      console.log(token)
      if (!token) {
          return next(new AppError('You are not logged in. Please log in to get access!', 401));
