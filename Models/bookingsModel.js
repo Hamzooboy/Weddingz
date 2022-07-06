f5a6c3aefae432943d31a5f89da92ebc4006cb7e
Hamzooboy <awanhamza102@gmail.com>
2022-07-07 00:12:43 +0500

    Bookings populate

--- Models/bookingsModel.js
+++ Models/bookingsModel.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const Venue = require("./venueModel");
 
-const bookingSchema = new mongoose.Schema(
-  {
+const bookingSchema = new mongoose.Schema({
     venue: {
-      type: mongoose.Schema.ObjectId,
-      ref: "Venues",
-      // required: [true, 'Booking must belong to a Venue']
+        type: mongoose.Schema.ObjectId,
+        ref: "Venues",
+        // required: [true, 'Booking must belong to a Venue']
     },
     // vendor: {
     //   type: mongoose.Schema.ObjectId,
@@ -14,40 +13,41 @@ const bookingSchema = new mongoose.Schema(
     //   // required: [true, 'Booking must belong to a Vendor']
     // },
     user: {
-      type: mongoose.Schema.ObjectId,
-      ref: "Users",
-      required: [true, "Booking must belong to a Customer"],
+        type: mongoose.Schema.ObjectId,
+        ref: "Users",
+        required: [true, "Booking must belong to a Customer"],
     },
     price: {
-      type: Number,
-      required: [true, 'A Booking must have a price']
+        type: Number,
+        required: [true, 'A Booking must have a price']
     },
     createdAt: {
-      type: String,
+        type: String,
     },
     status: {
-      type: String,
-      required: true,
-      default: "Booked",
+        type: String,
+        required: true,
+        default: "Booked",
     },
     slot: {
-      type: String,
-      enum: ["morning", "evening"],
-      default: "evening",
-      required: [true, "Booking slot is required"],
+        type: String,
+        enum: ["morning", "evening"],
+        default: "evening",
+        required: [true, "Booking slot is required"],
     },
-  },
-  {
+}, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
-);
-bookingSchema.pre(/^find/, function (next) {
-  this.populate("user").populate({
-    path: "venue",
-    select: "title",
-  });
-  next();
+});
+bookingSchema.pre(/^find/, function(next) {
+    this.populate({
+        path: 'user',
+        select: 'name'
+    }).populate({
+        path: "venue",
+        select: "title",
+    });
+    next();
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
